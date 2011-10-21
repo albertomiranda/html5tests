c1dec07081c91ae6d3ab486454d8c0cf34ec003b
Esteban S. Abait <estebanabait@gmail.com>
2011-10-21 13:07:01 -0300

    Added support for namespaces
    The generic namespace function was added to the Voxine object.
    The Global object has been refactored to follow the Module Pattern

--- js/default.js
+++ js/default.js
@@ -7,31 +7,59 @@
 //------------------------------------------------------------------------------
 //INIT
 $(document).ready(function(){
-    Global.isMobile();
+	Voxine.utilities.global.isMobile();
 });
 //------------------------------------------------------------------------------
 
-var Global = {
-    "isMobile": function(){
-        //mobile detection
-        var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  
-        if (mobile) {  
-            alert('Enjoy your mobile experience!');
-            // Hides mobile browser's address bar when page is done loading.
-            window.addEventListener('load', function(e) {
-                setTimeout(function() {window.scrollTo(0, 1);}, 1);
-            }, false);
-        }
-    },
+//------------------------------------------------------------------------------
+//Namespace support
+var Voxine = Voxine || {};
 
-    "checkCacheUpdate": function(){
-        window.applicationCache.addEventListener('updateready', function(e) {
-          if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-            window.applicationCache.swapCache();
-            if (confirm('A new version of this site is available. Load it?')) {
-              window.location.reload();
-            }
-          }
-        }, false);
-    }
+Voxine.namespace = function(ns_string) {
+	var parts = ns_string.split('.'),
+		parent = Voxine,
+		i;
+	
+	//Strip redundant leading global
+	if (parts[0] === 'Voxine') {
+		parts = parts.slice(1);
+	}
+	
+	for (i = 0; i < parts.length; i += 1) {
+		//create a property if it doesn't exist
+		if (typeof parent[parts[i]] === "undefined") {
+			parent[parts[i]] = {};
+		}
+		parent = parent[parts[i]];
+	}
+	return parent;
 };
+//------------------------------------------------------------------------------
+
+Voxine.namespace('Voxine.utilities.global');
+Voxine.utilities.global = (function() {
+	return {
+	    "isMobile": function(){
+	        //mobile detection
+	        var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));  
+	        if (mobile) {  
+	            alert('Enjoy your mobile experience!');
+	            // Hides mobile browser's address bar when page is done loading.
+	            window.addEventListener('load', function(e) {
+	                setTimeout(function() {window.scrollTo(0, 1);}, 1);
+	            }, false);
+	        }
+	    },
+	
+	    "checkCacheUpdate": function(){
+	        window.applicationCache.addEventListener('updateready', function(e) {
+	          if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+	            window.applicationCache.swapCache();
+	            if (confirm('A new version of this site is available. Load it?')) {
+	              window.location.reload();
+	            }
+	          }
+	        }, false);
+	    }
+	};   
+}());
