bd66e3c35eb9ebdc443340f9b0cf48004a3f8df3
Alberto Miranda <alberto@nextive.com>
2011-10-20 21:30:23 -0300

    added viewport meta

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta class="meta_disable_zoom" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <title><?php echo "$title: $subtitle"; ?></title>
         <link href="css/default.css" rel="stylesheet" />
         
