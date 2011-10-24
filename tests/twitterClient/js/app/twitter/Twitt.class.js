badf2b0e9bf7d315dc6d3257aad29fc2d579b470
Esteban S. Abait <estebanabait@gmail.com>
2011-10-24 11:35:15 -0300

    Added semicolons

--- tests/twitterClient/js/app/twitter/Twitt.class.js
+++ tests/twitterClient/js/app/twitter/Twitt.class.js
@@ -16,21 +16,21 @@ define(function (data) {
         this.getHtml = function(){
             var template = Voxine.templates.twitt;
             return Mustache.to_html(template, this.data);
-        }
+        };
         
-        this.created_at = data.created_at,
-        this.profile_image_url = data.profile_image_url,
-        this.from_user_id_str = data.from_user_id_str,
-        this.id_str = data.id_str,
-        this.from_user = data.from_user,
-        this.text = data.text,
-        this.to_user_id = data.to_user_id,
-        this.metadata = data.metadata,
-        this.id = data.id,
-        this.geo = data.geo,
-        this.from_user_id = data.from_user_id,
-        this.iso_language_code = data.iso_language_code,
-        this.source = data.source,
-        this.to_user_id_str = data.to_user_id_str
+        this.created_at = data.created_at;
+        this.profile_image_url = data.profile_image_url;
+        this.from_user_id_str = data.from_user_id_str;
+        this.id_str = data.id_str;
+        this.from_user = data.from_user;
+        this.text = data.text;
+        this.to_user_id = data.to_user_id;
+        this.metadata = data.metadata;
+        this.id = data.id;
+        this.geo = data.geo;
+        this.from_user_id = data.from_user_id;
+        this.iso_language_code = data.iso_language_code;
+        this.source = data.source;
+        this.to_user_id_str = data.to_user_id_str;
     };
 });
