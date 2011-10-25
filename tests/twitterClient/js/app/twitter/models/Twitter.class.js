dac2d8689bfd240e4e930eb23fada5e73b5f4c4e
Esteban S. Abait <estebanabait@gmail.com>
2011-10-25 17:20:57 -0300

    New method for getting users data from twitter. Modularization of ajax call code

--- tests/twitterClient/js/app/twitter/models/Twitter.class.js
+++ tests/twitterClient/js/app/twitter/models/Twitter.class.js
@@ -5,35 +5,68 @@ define("app/twitter/models/Twitter.class", ["jQuery"], function ($) {
 	 * This method makes a JSONP call to the
 	 * Twitter's API, and then calls a callback on the results.
 	 * 
-	 * Takes as input a config object, more parameters can
-	 * be added to reflect the Twitter API.
-	 * 
-	 * @method 	getTweets
-	 * @param 	Object {q: query}
+	 * @method 	ajaxCall
+	 * @param 	String url
 	 * @param 	Function callback
-	 * @return 	the result of applying the callback to the twitter call or
+	 * @return 	applies the callback to the results of the twitter call or
 	 * 		   	{error: errorThrown} in case of an error.
 	 */
-	var getTweets = function(searchConfig, callback) {
-		var q = searchConfig.q;
-		
+	var ajaxCall = function (url, callback) {
 		//Workaround to the "error callback" not called for JSONP
-		//
 		$.ajaxSetup({"error":function(XMLHttpRequest,textStatus, errorThrown) {   
 		    callback({error: errorThrown});
 		}});
 		
 		$.ajax({
 			type: 'GET',
-			url: "http://search.twitter.com/search.json?q=%40" + q, 
+			url: url, 
 			crossDomain: true,
 			success: function (data) {
 				callback(data);
 			},
 			dataType: 'jsonp'
 		});
-	}
+	};
+	
+	/** 
+	 * Get tweets from Twitter's real-time search API
+	 * 
+	 * Search arguments must be specified as following
+	 * searchConfig = {q: query, sinceId: tweetId}
+	 * 
+	 * @method 	getTweets
+	 * @param 	Object searchConfig
+	 * @param 	Function callback
+	 * @return  Applies a callback to a list of tweets.
+	 */
+	var getTweets = function(searchConfig, callback) {
+		var q = searchConfig.q;
+		var sinceId = searchConfig.sinceId;
+		var url = "http://search.twitter.com/search.json?q=%40" + q + "&since_id=" + sinceId;
+		this.ajaxCall(url, callback);
+	};
+	
+	 /** 
+	 * UNTESTED METHOD
+	 * 
+	 * Return up to 100 users worth of extended information
+	 * 
+	 * Search arguments must be specified as following
+	 * searchConfig = {q: [userID | username | both] }
+	 * 
+	 * @method 	getTweets
+	 * @param 	Object searchConfig
+	 * @param 	Function callback
+	 * @return  Applies a callback to a list of tweets.
+	 */
+	var getUser = function(searchConfig, callback) {
+		var q = searchConfig.q;
+		var url = "http://api.twitter.com/1/users/lookup.json?q=%40" + q;
+		this.ajaxCall(url, callback);
+	};
+	
 	return {
-		getTweets : getTweets
+		getTweets : getTweets,
+		getUser : getUser
 	}
 });
