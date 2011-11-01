05d79af1073bd5b817c3e467e2e11c88d189d094
Esteban S. Abait <estebanabait@gmail.com>
2011-11-01 15:07:05 -0300

    Added VoxMediator.class to voxine/core

--- tests/twitterClient/js/app.js
+++ tests/twitterClient/js/app.js
@@ -1,11 +1,12 @@
 // Filename: app.js
 define([
   'VoxClass',
+  "voxine/core/VoxMediator.class",
   'jQuery',
   "voxine/tools/Tools.class",
   "app/Bindings.class",
   "lib/mustache"
-], function(VoxClass, $, Tools, Bindings) {
+], function(VoxClass, VoxMediator, $, Tools, Bindings) {
     return VoxClass.Class(
         'App',
         null,
@@ -21,6 +22,22 @@ define([
                     ToolsInstance = new Tools();
                     if(ToolsInstance.isMobile()) ToolsInstance.welcomeMobile();
                     //ToolsInstance.runTests();
+                    
+                    var pub = {}, sub = {name: 'Pedro'};
+                    var mediator = new VoxMediator();
+                    
+                    //mix mediator into pub object
+                    mediator.mixin(pub);
+                    
+                    //bind (event, callback, context)
+                    var callback = function(){alert(this.name);};
+                    pub.bind('helloworld', callback, sub);
+                    
+                    //trigger(event)
+                    pub.trigger('helloworld');
+                    
+                    pub.unbind('helloworld', callback);
+                    pub.trigger('helloworld');
                 });
             }
         })
