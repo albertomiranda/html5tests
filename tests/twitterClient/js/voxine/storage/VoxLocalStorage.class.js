ea2192ced773a8085393ce77de584fb364d41a1a
Esteban S. Abait <estebanabait@gmail.com>
2011-11-01 18:33:58 -0300

    Comment on graceful degradation in VoxLocalStorage

--- tests/twitterClient/js/voxine/storage/VoxLocalStorage.class.js
+++ tests/twitterClient/js/voxine/storage/VoxLocalStorage.class.js
@@ -14,6 +14,14 @@ define([
  * PRIVATE----------------------------------------------------------
  */
         
+    /**
+     * Una de las cosas que tenés que hacer es testear si el dispositivo
+     * soporta localStorage, para eso tenés que usar Modernizer, yo después hago
+     * un wrapper así podés usar sus funciones acá adentro.
+     * En caso de que el dispositivo/browser no soporte localStorage podríamos degradar a
+     * cookies, o algo así. Investigar más sobre este punto.
+     */
+    
         var persist = function(key, securedObject) {
             window.localStorage.setItem(key, securedObject);
         };
