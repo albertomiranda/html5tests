e4eb27c55d00866bf25b0ae99c927c32ec1e5c17
Alberto Miranda <alberto@nextive.com>
2011-11-01 15:09:02 -0300

    added private property

--- tests/practice/jsTester/samples/car.class.js
+++ tests/practice/jsTester/samples/car.class.js
@@ -1,5 +1,8 @@
 var Car = (function(){
+    var privateKey = 'im private!';
+
     return function(color, cilinders){
+        console.log("PRIVATE KEY: " + privateKey);
         console.log("CONSTRUCTOR: color: " + color);
         this.color = color;
         this.cilinders = cilinders;
@@ -12,3 +15,4 @@ var Car = (function(){
 
 var car = new Car('red', 4);
 console.log(car.start());
+console.log(car.privateKey);
