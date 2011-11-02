974abd90b8ee04e77f99a6e49694d9e1aede4186
Esteban S. Abait <estebanabait@gmail.com>
2011-11-02 10:21:27 -0300

    Add Modernizr module wrapper

--- tests/twitterClient/js/app.js
+++ tests/twitterClient/js/app.js
@@ -25,7 +25,7 @@ define([
                     ToolsInstance = new Tools();
                     if(ToolsInstance.isMobile()) ToolsInstance.welcomeMobile();
                     //ToolsInstance.runTests();
-                    
+
                     //----------------------------------------------------------
                     //run tests
                     //VoxTests.mediator();
