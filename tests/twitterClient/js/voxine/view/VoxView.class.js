468120daefd2a02d8a2b11be5f237ddaa244f9ef
Esteban S. Abait <estebanabait@gmail.com>
2011-11-02 16:24:19 -0300

    Fixed context issue in VoxView

--- tests/twitterClient/js/voxine/view/VoxView.class.js
+++ tests/twitterClient/js/voxine/view/VoxView.class.js
@@ -12,69 +12,6 @@ define(
         'voxine/core/VoxMediator.class'
     ],
     function(VoxClass, Mustache, VoxMediator) {
-        var private = {
-            "template": null,
-            "target": null,
-            "caller": null,
-            "data": null
-        };
-        
-        /**
-         * Renders template inside target element using passed data.
-         * If no target was specified returns output.
-         * 
-         * @author Alberto Miranda <alberto@nextive.com>
-         * @param object data
-         * @return string
-         */
-        var render = function(data) {
-            private.data = data;
-            console.log(private);
-            
-            //console.log(data);
-            //if no data was specified handle error
-            if (data == undefined) {
-                console.log("RENDER ERROR: NO DATA"); //TODO: implement error handling system
-            } 
-            
-            //if no template was specified handle error
-            if (private.template == null || private.template == undefined) {
-                console.log("RENDER ERROR: NO TEMPLATE"); //TODO: implement error handling system
-            }
-            
-            //add mediator
-            var Mediator = new VoxMediator();
-            Mediator.mixin(this); 
-            this.bind('viewLoaded', function(template){
-                console.log(private); return false;
-                var output = Mustache.to_html(template, this.data);
-                    
-                console.log(this.data.testName + ": TARGET: " + this.target);
-                return false;
-
-                //if no target was specified and caller uses trigger,
-                //trigger template loaded event
-                if (private.target == null || private.target == undefined) {
-                    if(private.caller.trigger !== undefined) {
-                        private.caller.trigger('parsed', output);
-                    }
-                }
-
-                //assign output to target element
-                $(private.target).html(output);
-            }, private);
-            
-            var View = this;
-            $.ajax({
-                url: 'js/app/views/' + private.template,
-                success: function(template){
-                    //console.log(private);
-                    View.trigger('viewLoaded', template);
-                }
-            });
-        };
-        
-        //Return the Class
         return VoxClass.Class(
             'VoxView',
             null,
@@ -94,15 +31,40 @@ define(
                     }
 
                     //set private properties
-                    private.template = template;
-                    private.caller = caller;
-                    private.target = target;
+                    this.template = template;
+                    this.caller = caller;
+                    this.target = target;
 
                     //public properties
                     this.test = target;
-
-                    //public methods
-                    this.render = render;
+                },
+                render : function(data) {
+                    this.data = data;
+                    //console.log('render ' + private);
+                    
+                    //console.log(data);
+                    //if no data was specified handle error
+                    if (data == undefined) {
+                        console.log("RENDER ERROR: NO DATA"); //TODO: implement error handling system
+                    } 
+                    
+                    //if no template was specified handle error
+                    if (this.template == null || this.template == undefined) {
+                        console.log("RENDER ERROR: NO TEMPLATE"); //TODO: implement error handling system
+                    }
+                    
+                    var data = this.data;
+                    var target = this.target;
+                    
+                    var onLoadTemplate = function(template){
+                        var output = Mustache.to_html(template, this.data);          
+                        console.log(target);
+                    };
+                    
+                    $.ajax({
+                        url: 'js/app/views/' + this.template,
+                        success: onLoadTemplate
+                    });
                 }
             }
         ); 
