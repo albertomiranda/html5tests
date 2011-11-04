14f3aa31b11b2abe6d135560ec9a88d5dcfbff7f
Alberto Miranda <alberto@nextive.com>
2011-11-03 23:58:07 -0300

    working app config sample

--- tests/twitterClient/js/app/config/AppConfig.js
+++ tests/twitterClient/js/app/config/AppConfig.js
@@ -1,17 +1,31 @@
 define([    
-    'VoxClass'
+    'VoxClass',
+    'voxine/tools/VoxTools.class'
     ], 
-    function(VoxClass) {
-        var config = {
-            app: {
-                setting1 : 'value setting 1'
-            },
-            voxine: {
-                hosts : {
-                    Esteban : "Q170",
-                    Paul: "Ha7man"
-                }        
+    function(VoxClass, VoxTools) {
+        //default config
+        var appDefaultConfig = {
+            "setting1" : "value setting 1"
+        };
+        
+        //app custom hosts config
+        var appHostsConfig = {
+            "localhost": {
+                "Esteban" : "Q170",
+                "Paul": "Ha7man"
             }
-        }
+        };
+        
+        //MERGE CONFIGS
+        var host = 'localhost'; //test, TODO: obtain actual hostname
+        var tools = new VoxTools;
+        var config = tools.mergeObject(appDefaultConfig, appHostsConfig[host]);
+        
+        //----------------------------------------------------------------------
+        /**
+         * PUBLIC INTERFACE
+         */
+        return config;
+        //----------------------------------------------------------------------
     }
 );
